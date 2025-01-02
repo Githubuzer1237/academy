6e346852783b2c89fa3436b3385b1c87145b22a7
Azim <158813082+Githubuzer1237@users.noreply.github.com>
2025-01-02 19:19:24 +0500

    Add files via upload

--- src/pages/StudioPage.jsx
+++ src/pages/StudioPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Studio from '../components/Studio/Studio'
 import Wrapper from '../components/Wrapper/Wrapper'
+import Team from '../components/Team/Team'
 
 const StudioPage = () => {
   return (
@@ -8,6 +9,7 @@ const StudioPage = () => {
     <Studio />
     <Wrapper title='Наша студия' maintitle='Креативная площадка'
      text='Созданная с нуля, студия It Academy была спроектирована и создана для развития творческого сообщества, которая вдохновляет учеников усердно заниматься. Наша студия, расположенная в ГЛАВПОЧТАМПТЕ, воплощает наш идеал «Серьезно игривый» — серьезный спереди, игривый сзади.' linkspan='It Academy' link='/' />
+     <Team />
     </>
   )
 }
